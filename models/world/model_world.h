ae0e11e1b4917a994d2d7670fe09513b8c66bb42
miluaces <miluaces@yahoo.com>
2020-11-08 19:35:14 -0700

    Added world model and collisions for testing

---
+++ models/world/model_world.h
@@ -0,0 +1,25 @@
+extern Vtx world_Skybox_mesh_vtx_0[4];
+extern Gfx world_Skybox_mesh_tri_0[];
+extern Vtx world_Skybox_mesh_vtx_1[4];
+extern Gfx world_Skybox_mesh_tri_1[];
+extern Vtx world_Skybox_mesh_vtx_2[4];
+extern Gfx world_Skybox_mesh_tri_2[];
+extern Vtx world_Skybox_mesh_vtx_3[4];
+extern Gfx world_Skybox_mesh_tri_3[];
+extern Vtx world_Skybox_mesh_vtx_4[4];
+extern Gfx world_Skybox_mesh_tri_4[];
+extern Vtx world_Skybox_mesh_vtx_5[48];
+extern Gfx world_Skybox_mesh_tri_5[];
+extern Vtx world_Skybox_mesh_vtx_6[80];
+extern Gfx world_Skybox_mesh_tri_6[];
+extern Vtx world_Skybox_mesh_vtx_7[160];
+extern Gfx world_Skybox_mesh_tri_7[];
+extern Vtx world_Skybox_mesh_vtx_8[56];
+extern Gfx world_Skybox_mesh_tri_8[];
+extern Vtx world_Skybox_mesh_vtx_9[8];
+extern Gfx world_Skybox_mesh_tri_9[];
+extern Vtx world_Skybox_mesh_vtx_10[32];
+extern Gfx world_Skybox_mesh_tri_10[];
+
+extern Gfx world_Skybox_mesh[];
+
---
+++ models/world/model_world_col.h
@@ -0,0 +1,1 @@
+extern const Collision worldcol_collision[];
