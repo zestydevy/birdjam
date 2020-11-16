11970bc4bb9871d4fb86191736dd15846bef2d06
miluaces <miluaces@yahoo.com>
2020-11-16 13:23:05 -0700

    Separated playable area from distant area in world model.

---
+++ models/world/model_distant.h
@@ -0,0 +1,11 @@
+extern Vtx distant_Distant_mesh_vtx_0[8];
+extern Gfx distant_Distant_mesh_tri_0[];
+extern Vtx distant_Distant_mesh_vtx_1[41];
+extern Gfx distant_Distant_mesh_tri_1[];
+extern Vtx distant_Distant_mesh_vtx_2[20];
+extern Gfx distant_Distant_mesh_tri_2[];
+extern Vtx distant_Distant_mesh_vtx_3[8];
+extern Gfx distant_Distant_mesh_tri_3[];
+
+extern Gfx distant_Distant_mesh[];
+
--- models/world/model_world.h
+++ models/world/model_world.h
@@ -1,14 +1,14 @@
-extern Vtx world_World_mesh_vtx_0[198];
+extern Vtx world_World_mesh_vtx_0[155];
 extern Gfx world_World_mesh_tri_0[];
 extern Vtx world_World_mesh_vtx_1[48];
 extern Gfx world_World_mesh_tri_1[];
 extern Vtx world_World_mesh_vtx_2[80];
 extern Gfx world_World_mesh_tri_2[];
-extern Vtx world_World_mesh_vtx_3[56];
+extern Vtx world_World_mesh_vtx_3[36];
 extern Gfx world_World_mesh_tri_3[];
 extern Vtx world_World_mesh_vtx_4[8];
 extern Gfx world_World_mesh_tri_4[];
-extern Vtx world_World_mesh_vtx_5[32];
+extern Vtx world_World_mesh_vtx_5[24];
 extern Gfx world_World_mesh_tri_5[];
 extern Vtx world_World_mesh_vtx_6[150];
 extern Gfx world_World_mesh_tri_6[];
@@ -16,8 +16,6 @@ extern Vtx world_World_mesh_vtx_7[12];
 extern Gfx world_World_mesh_tri_7[];
 extern Vtx world_World_mesh_vtx_8[4];
 extern Gfx world_World_mesh_tri_8[];
-extern Vtx world_World_mesh_vtx_9[8];
-extern Gfx world_World_mesh_tri_9[];
 
 extern Gfx world_World_mesh[];
 
