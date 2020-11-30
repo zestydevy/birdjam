3ac42b9800dbe1f11e87ef8d9b896e08807842c9
zestydevy <zestydevy@gmail.com>
2020-11-30 13:36:26 -0600

    TUtil

--- include/util.hpp
+++ include/util.hpp
@@ -4,3 +4,10 @@
 
 float constexpr kFrameRate = 60.0f;
 float constexpr kInterval = 1.0f / kFrameRate;
+
+class TUtil
+{
+    public:
+
+    static void toMemory(void * dest, void * src, s32 size);
+};
---
+++ src/util.cpp
@@ -0,0 +1,16 @@
+#include <nusys.h>
+
+#include "util.hpp"
+
+// -------------------------------------------------------------------------- //
+
+void TUtil::toMemory(void * dest, void * src, s32 size)
+{
+    // transfer size can't be an odd number
+    if (size & 1) {
+        ++size;
+    }
+    nuPiReadRom(reinterpret_cast<u32>(src), dest, size);
+}
+
+// -------------------------------------------------------------------------- //
