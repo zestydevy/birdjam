d9f449b5619731d053ff71ecabd7cfaa420bbfcf
arookas <arookas@outlook.com>
2020-12-05 21:10:46 -0500

    Break sprites lmao

--- include/sprite.hpp
+++ include/sprite.hpp
@@ -2,9 +2,9 @@
 
 #include <nusys.h>
 
+#include "array.hpp"
 #include "dynlist.hpp"
 #include "math.hpp"
-#include "app.hpp"
 
 // -------------------------------------------------------------------------- //
 
@@ -20,61 +20,47 @@ extern Gfx rspinit_dl[];
 extern Gfx rdpinit_dl[];
 extern Gfx rdpinit_spr_dl[];
 
-class TSprite
-{
-    public:
-    
-    TSprite(Sprite * sprite, s32 x, s32 y)
-    {
-      // copy sprite so we no longer have to make copies
-      mSprite = new Sprite;
-      *mSprite = *sprite;
-      
-      mSpriteDL = new Gfx[mSprite->ndisplist];
-      mSprite->rsp_dl = mSpriteDL;
+class TSprite {
 
-      mPosition.x() = x;
-      mPosition.y() = y;
-    }
-    ~TSprite()
-    {
-        delete mSprite;
-        delete[] mSpriteDL;
-    }
+    public:
+
+    TSprite() = default;
+    ~TSprite() = default;
+
+    void load(Sprite const &);
+    void preload(Sprite const &);
+
+    TVec2S const & getPosition() const;
+    void setPosition(TVec2S const &);
+
+    TVec2F const & getScale() const;
+    void setScale(TVec2F const &);
+
+    TColor const & getColor() const;
+    void setColor(TColor const &);
+
+    u32 getAttributes() const;
+    void setAttributes(u32);
+    void setOnAttributes(u32);
+    void setOffAttributes(u32);
+
+    void draw();
 
     // initializes S2DEX. call before utilizing sprites
-    static inline void init(TDynList2 * list)
-    {
-        sDynList = list;
-        auto dl = sDynList->getDL();
-        spInit(&dl);
-    }
+    static void init(TDynList2 *);
 
     // call after sprites are finished rendering.
-    static inline void finalize()
-    {
-        auto dl = sDynList->getDL();
-        gSPDisplayList(dl, rdpinit_dl);
-	    gSPDisplayList(dl, rspinit_dl);
-    }
-
-    void setPosition(TVec3S const & pos);
-    void setScale(TVec3F const & scale);
-    void setColor(TColor const & color);
-    void setAttribute(u32 const attrib);
-    void setTexture(void * buffer);
-
-    void render();
-
-    TVec2<s32> mPosition{};
-    TVec2<float> mScale{};
-    TColor mColor{255};
-    u32 mAttributes{SP_FASTCOPY};
+    static void finalize();
 
     private:
-    
-    Sprite * mSprite{nullptr};
-    Gfx * mSpriteDL{nullptr};
+
+    TVec2S mPosition { 0, 0 };
+    TVec2F mScale { 1.0F, 1.0F };
+    TColor mColor { 255, 255, 255, 255};
+    u32 mAttributes { SP_FASTCOPY };
+    Sprite mSpriteData;
+    TArray<Gfx> mSpriteGfx;
+
     static TDynList2 * sDynList;
 
 };
--- src/sprite.cpp
+++ src/sprite.cpp
@@ -1,50 +1,137 @@
+
 #include <nusys.h>
 
+#include "array.hpp"
+#include "dynlist.hpp"
 #include "sprite.hpp"
 
 // -------------------------------------------------------------------------- //
 
-TDynList2 * TSprite::sDynList{nullptr};
+TDynList2 * TSprite::sDynList { nullptr };
 
 // -------------------------------------------------------------------------- //
 
-void TSprite::setPosition(TVec3S const & pos)
-{
-    mPosition.x() = pos.x(); 
-    mPosition.y() = pos.y();
+void TSprite::load(
+    Sprite const & sprite
+) {
+    mSpriteData = sprite;
+    mSpriteGfx.resize((u32)sprite.ndisplist);
+    mSpriteData.rsp_dl = mSpriteGfx.data();
 }
 
-void TSprite::setScale(TVec3F const & scale)
-{
-    mScale.x() = scale.x();
-    mScale.y() = scale.y(); 
+// -------------------------------------------------------------------------- //
+
+void TSprite::preload(
+    Sprite const & sprite
+) {
+    auto size = (u32)sprite.ndisplist;
+
+    if (size > mSpriteGfx.size()) {
+        mSpriteGfx.resize(size);
+        mSpriteData.rsp_dl = mSpriteGfx.data();
+    }
 }
 
-void TSprite::setColor(TColor const & color)
-{
+// -------------------------------------------------------------------------- //
+
+TVec2S const &
+TSprite::getPosition() const {
+    return mPosition;
+}
+
+// -------------------------------------------------------------------------- //
+
+void TSprite::setPosition(
+    TVec2S const & pos
+) {
+    mPosition = pos;
+}
+
+// -------------------------------------------------------------------------- //
+
+TVec2F const &
+TSprite::getScale() const {
+    return mScale;
+}
+
+// -------------------------------------------------------------------------- //
+
+void TSprite::setScale(
+    TVec2F const & scale
+) {
+    mScale = scale;
+}
+
+// -------------------------------------------------------------------------- //
+
+TColor const &
+TSprite::getColor() const {
+    return mColor;
+}
+
+// -------------------------------------------------------------------------- //
+
+void TSprite::setColor(
+    TColor const & color
+) {
     mColor = color;
 }
 
-void TSprite::setAttribute(u32 const attrib)
-{
+// -------------------------------------------------------------------------- //
+
+u32 TSprite::getAttributes() const {
+    return mAttributes;
+}
+
+// -------------------------------------------------------------------------- //
+
+void TSprite::setAttributes(
+    u32 const attrib
+) {
     mAttributes = attrib;
 }
 
-void TSprite::setTexture(void * buffer)
-{
-    mSprite->bitmap->buf = buffer;
+// -------------------------------------------------------------------------- //
+
+void TSprite::setOnAttributes(
+    u32 const mask
+) {
+    mAttributes |= mask;
 }
 
-void TSprite::render()
-{
-    mSprite->rsp_dl_next = mSprite->rsp_dl;
-    
-    spMove(mSprite, mPosition.x(), mPosition.y());
-    spScale(mSprite, mScale.x(), mScale.y());
-    spColor(mSprite, mColor.r, mColor.g, mColor.b, mColor.a);
-    spSetAttribute( mSprite, mAttributes);
-    
-    gSPDisplayList(sDynList->pushDL(), spDraw(mSprite));
+// -------------------------------------------------------------------------- //
+
+void TSprite::setOffAttributes(
+    u32 const mask
+) {
+    mAttributes &= ~mask;
+}
+
+// -------------------------------------------------------------------------- //
+
+void TSprite::draw() {
+    mSpriteData.rsp_dl_next = mSpriteData.rsp_dl;
+    spMove(&mSpriteData, mPosition.x(), mPosition.y());
+    spScale(&mSpriteData, mScale.x(), mScale.y());
+    spColor(&mSpriteData, mColor.r, mColor.g, mColor.b, mColor.a);
+    spSetAttribute(&mSpriteData, mAttributes);
+    gSPDisplayList(sDynList->pushDL(), spDraw(&mSpriteData));
+}
+
+// -------------------------------------------------------------------------- //
+
+void TSprite::init(TDynList2 * dl) {
+    sDynList = dl;
+    auto gfx = sDynList->getDL();
+    spInit(&gfx);
+}
+
+// -------------------------------------------------------------------------- //
+
+void TSprite::finalize() {
+    auto dl = sDynList->getDL();
+    gSPDisplayList(dl, rdpinit_dl);
+    gSPDisplayList(dl, rspinit_dl);
 }
 
 // -------------------------------------------------------------------------- //
