2817d98890e56bfb36c6601c9a7e74988c09611f
arookas <arookas@outlook.com>
2020-12-11 22:56:53 -0500

    Add menu sprites

---
+++ models/ovl/sprites/sp_menu.h
@@ -0,0 +1,127 @@
+
+#ifndef INCLUDE_SP_MENU_H
+#define INCLUDE_SP_MENU_H
+
+#include <PR/sp.h>
+
+// -------------------------------------------------------------------------- //
+// @menu_logo
+
+#define menu_logoTRUEIMAGEH	83
+#define menu_logoTRUEIMAGEW	280
+#define menu_logoIMAGEH	96
+#define menu_logoIMAGEW	288
+#define menu_logoBLOCKSIZEW	32
+#define menu_logoBLOCKSIZEH	32
+#define menu_logoSCALEX	1.0
+#define menu_logoSCALEY	1.0
+#define menu_logoMODE	SP_Z | SP_OVERLAP | SP_TRANSPARENT
+
+extern Bitmap menu_logo_bitmaps[];
+extern Gfx menu_logo_dl[];
+
+#define NUM_menu_logo_BMS  (sizeof(menu_logo_bitmaps)/sizeof(Bitmap))
+
+extern Sprite menu_logo_sprite;
+
+// -------------------------------------------------------------------------- //
+// @menu_bird
+
+#define menu_birdTRUEIMAGEH	63
+#define menu_birdTRUEIMAGEW	93
+#define menu_birdIMAGEH	64
+#define menu_birdIMAGEW	96
+#define menu_birdBLOCKSIZEW	32
+#define menu_birdBLOCKSIZEH	32
+#define menu_birdSCALEX	1.0
+#define menu_birdSCALEY	1.0
+#define menu_birdMODE	SP_Z | SP_OVERLAP | SP_TRANSPARENT
+
+extern Bitmap menu_bird_bitmaps[];
+extern Gfx menu_bird_dl[];
+
+#define NUM_menu_bird_BMS  (sizeof(menu_bird_bitmaps)/sizeof(Bitmap))
+
+extern Sprite menu_bird_sprite;
+
+// -------------------------------------------------------------------------- //
+// @menu_swoop
+
+#define menu_swoopTRUEIMAGEH	32
+#define menu_swoopTRUEIMAGEW	230
+#define menu_swoopIMAGEH	32
+#define menu_swoopIMAGEW	256
+#define menu_swoopBLOCKSIZEW	32
+#define menu_swoopBLOCKSIZEH	32
+#define menu_swoopSCALEX	1.0
+#define menu_swoopSCALEY	1.0
+#define menu_swoopMODE	SP_Z | SP_OVERLAP | SP_TRANSPARENT
+
+extern Bitmap menu_swoop_bitmaps[];
+extern Gfx menu_swoop_dl[];
+
+#define NUM_menu_swoop_BMS  (sizeof(menu_swoop_bitmaps)/sizeof(Bitmap))
+
+extern Sprite menu_swoop_sprite;
+
+// -------------------------------------------------------------------------- //
+// @menu_practice
+
+#define menu_practiceTRUEIMAGEH	57
+#define menu_practiceTRUEIMAGEW	159
+#define menu_practiceIMAGEH	64
+#define menu_practiceIMAGEW	160
+#define menu_practiceBLOCKSIZEW	32
+#define menu_practiceBLOCKSIZEH	32
+#define menu_practiceSCALEX	1.0
+#define menu_practiceSCALEY	1.0
+#define menu_practiceMODE	SP_Z | SP_OVERLAP | SP_TRANSPARENT
+
+extern Bitmap menu_practice_bitmaps[];
+extern Gfx menu_practice_dl[];
+
+#define NUM_menu_practice_BMS  (sizeof(menu_practice_bitmaps)/sizeof(Bitmap))
+
+extern Sprite menu_practice_sprite;
+
+// -------------------------------------------------------------------------- //
+// @menu_time
+
+#define menu_timeTRUEIMAGEH	57
+#define menu_timeTRUEIMAGEW	133
+#define menu_timeIMAGEH	64
+#define menu_timeIMAGEW	160
+#define menu_timeBLOCKSIZEW	32
+#define menu_timeBLOCKSIZEH	32
+#define menu_timeSCALEX	1.0
+#define menu_timeSCALEY	1.0
+#define menu_timeMODE	SP_Z | SP_OVERLAP | SP_TRANSPARENT
+
+extern Bitmap menu_time_bitmaps[];
+extern Gfx menu_time_dl[];
+
+#define NUM_menu_time_BMS  (sizeof(menu_time_bitmaps)/sizeof(Bitmap))
+
+extern Sprite menu_time_sprite;
+
+// -------------------------------------------------------------------------- //
+// @menu_freedom
+
+#define menu_freedomTRUEIMAGEH	57
+#define menu_freedomTRUEIMAGEW	155
+#define menu_freedomIMAGEH	64
+#define menu_freedomIMAGEW	160
+#define menu_freedomBLOCKSIZEW	32
+#define menu_freedomBLOCKSIZEH	32
+#define menu_freedomSCALEX	1.0
+#define menu_freedomSCALEY	1.0
+#define menu_freedomMODE	SP_Z | SP_OVERLAP | SP_TRANSPARENT
+
+extern Bitmap menu_freedom_bitmaps[];
+extern Gfx menu_freedom_dl[];
+
+#define NUM_menu_freedom_BMS  (sizeof(menu_freedom_bitmaps)/sizeof(Bitmap))
+
+extern Sprite menu_freedom_sprite;
+
+// -------------------------------------------------------------------------- //
